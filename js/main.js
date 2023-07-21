ac586ee5342d6bf132b06c3b8067da6dd56de573
Ti-KM <98748234+Ti-KM@users.noreply.github.com>
2023-07-21 15:57:05 +0900

    test

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// ハンバーガーメニューの表示
+$(function(){
+    $(".menu-button").click(function() {
+        $(this).toggleClass("close-button");
+        $(".menu").toggle();
+        $(".site-menu").toggle();
+    });
+});
+
+// ページトップへ
+$(document).ready(function() {
+    var pagetop = $('.pagetop');
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 300) {//どの程度スクロールしたらボタンが現れるか
+        pagetop.fadeIn();
+      } else {
+        pagetop.fadeOut();
+      }
+    });
+    pagetop.click(function () {
+      $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+  });
